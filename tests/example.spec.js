27f5c922d11ab3cbd45953aaf910add9e4c18098
NadejdaF <84542803+NadejdaF@users.noreply.github.com>
2021-07-15 21:42:50 -0400

    Env vars (#3)
    * add environment variables
    add setup config
    update .gitignore
    
    * delete idea

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,14 @@
+import {expect} from "chai";
 
-
-describe('operations with numbers', function() {
+describe('operations with numbers',  function() {
     const a = 5;
     const b = 7;
 
-    it('addition works correctly', function() {
-        expect(a + b, 'result should equal 12').to.eq(12);
+    it('addition works correctly',  function() {
+             expect( a + b, 'result should equal 12').to.eq(12);
     });
-    it('subtraction works correctly'), function() {
-        expect(a - b, 'result should equal -2').to.eq(-2);
-    };
 
+    it('substraction works correctly', function() {
+        expect( a - b, 'result should equal -2').to.eq(-2);
+    });
 });
